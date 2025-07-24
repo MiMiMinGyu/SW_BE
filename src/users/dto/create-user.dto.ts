41e831570c413929bc6643e3389919199bd8df97
jinseojang0903 <jinseojang4@gmail.com>
2025-07-24 21:30:02 +0900

    유효성 검사 메시지 추가 및 파일 기본값 최종 수정본

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,17 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @IsEmail() //이메일 형식인지
+  //이메일 형식인지
+  @IsEmail({}, { message: '올바른 이메일 형식을 입력해주세요.' })
   email: string;
 
-  @IsString()
-  @MinLength(8) //최소 8자인지
+  //비밀번호가 최소 8자인지
+  @IsString({ message: '비밀번호는 문자열이어야 합니다.' })
+  @MinLength(8, { message: '비밀번호는 8자 이상이어야 합니다.' })
   password: string;
 
-  @IsString()
-  @MinLength(2)
+  //닉네임이 최소 2글자인지
+  @IsString({ message: '닉네임은 문자열이어야 합니다.' })
+  @MinLength(2, { message: '닉네임은 2자 이상이어야 합니다.' })
   nickname: string;
 }
