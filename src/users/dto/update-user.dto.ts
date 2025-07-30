fe1002e968921c11873243d391979b4ada8a7382
jinseojang0903 <jinseojang4@gmail.com>
2025-07-30 14:49:48 +0900

    Feat: 내 작물 crud 기능 구현

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -3,11 +3,11 @@ import { IsOptional, IsString, MinLength } from 'class-validator';
 export class UpdateUserDto {
   @IsString({ message: '닉네임은 문자열이어야 합니다.' })
   @MinLength(2, { message: '닉네임은 2자 이상이어야 합니다.' })
-  @IsOptional() // 닉네임은 선택적으로 보낼 수 있음을 의미
+  @IsOptional()
   nickname?: string;
 
   @IsString({ message: '비밀번호는 문자열이어야 합니다.' })
   @MinLength(8, { message: '비밀번호는 8자 이상이어야 합니다.' })
-  @IsOptional() // 비밀번호는 선택적으로 보낼 수 있음을 의미
+  @IsOptional()
   password?: string;
 }
